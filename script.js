51e39f6fdbd201bad5e2577efef16e6bcb64659f
Adriana Kramer <adri.kramer@gmail.com>
2022-02-20 11:21:41 +0100

    Update createGrid.. function to use inline/block syntax

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-function createGrid(numSquares){
+const gridSize = 960;
+
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.height = `${gridSize}px`;
+gridContainer.style.width = `${gridSize}px`;
+
+createGridInlineBlock(16);
+
+function createGridInlineBlock(numSquares){
     for (let i = 0; i < numSquares; i++){
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridRow.classList.add('grid');
         
-        gridRow.style.height = `${960/numSquares}px`;
+        gridRow.style.height = `${gridSize/numSquares}px`;
         
         for (let j = 0; j < numSquares; j++){
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
             gridItem.classList.add('grid');
 
-            gridItem.style.height = `${960/numSquares}px`;
-            gridItem.style.width = `${960/numSquares}px`;
+            gridItem.style.height = `${gridSize/numSquares}px`;
+            gridItem.style.width = `${gridSize/numSquares}px`;
 
             gridRow.appendChild(gridItem);
         }
 
-        const gridContainer = document.querySelector('.grid-container');
         gridContainer.appendChild(gridRow);
     }
 }
-
-createGrid(16);
