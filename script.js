6d78ae5be8457c50261ec5f4bcba283198323b08
Adriana Kramer <adri.kramer@gmail.com>
2022-02-20 15:31:32 +0100

    Implement createGrid using flexbox (final version)

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
+const gridSize = 960;
+
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.height = `${gridSize}px`;
+gridContainer.style.width = `${gridSize}px`;
+
+createGrid(100);
+
 function createGrid(numSquares){
     for (let i = 0; i < numSquares; i++){
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridRow.classList.add('grid');
         
-        gridRow.style.height = `${960/numSquares}px`;
-        
         for (let j = 0; j < numSquares; j++){
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
             gridItem.classList.add('grid');
 
-            gridItem.style.height = `${960/numSquares}px`;
-            gridItem.style.width = `${960/numSquares}px`;
-
             gridRow.appendChild(gridItem);
         }
 
-        const gridContainer = document.querySelector('.grid-container');
         gridContainer.appendChild(gridRow);
     }
 }
-
-createGrid(16);
