7e0ad24a1a06557e076047c944d11b18cb98171a
Adriana Kramer <adri.kramer@gmail.com>
2022-02-20 15:17:30 +0100

    Update createGrid.. function to use CSS grid

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ const gridContainer = document.querySelector('.grid-container');
 gridContainer.style.height = `${gridSize}px`;
 gridContainer.style.width = `${gridSize}px`;
 
-createGridInlineBlock(16);
+createGridCSSGrid(100);
 
-function createGridInlineBlock(numSquares){
-    for (let i = 0; i < numSquares; i++){
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('grid-row');
-        gridRow.classList.add('grid');
-        
-        gridRow.style.height = `${gridSize/numSquares}px`;
-        
+function createGridCSSGrid(numSquares){
+    let gridTemplateCols = "";
+    for (let i = 0; i < numSquares; i++){    
+        gridTemplateCols += "auto ";
         for (let j = 0; j < numSquares; j++){
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
@@ -22,9 +18,8 @@ function createGridInlineBlock(numSquares){
             gridItem.style.height = `${gridSize/numSquares}px`;
             gridItem.style.width = `${gridSize/numSquares}px`;
 
-            gridRow.appendChild(gridItem);
+            gridContainer.appendChild(gridItem);
         }
-
-        gridContainer.appendChild(gridRow);
     }
+    gridContainer.style.gridTemplateColumns = gridTemplateCols;
 }
