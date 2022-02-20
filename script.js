165e90733dcc4e9029bc173160f8c73b3487852d
Adriana Kramer <adri.kramer@gmail.com>
2022-02-20 16:42:19 +0100

    Add functionality to change grid color after hovering

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ const gridContainer = document.querySelector('.grid-container');
 gridContainer.style.height = `${gridSize}px`;
 gridContainer.style.width = `${gridSize}px`;
 
-createGrid(100);
+createGrid(16);
+
+const gridItems = Array.from(document.querySelectorAll('.grid-item'));
+gridItems.forEach(item => item.addEventListener('mouseenter', changeColor));
+
+function changeColor(e){
+    const classes = Array.from(e.target.classList);
+    if (classes[2] === undefined){
+        e.target.classList.add('hovered-1');
+    } else {
+        const hoveredClass = classes[2];
+        const timesHovered = parseInt(hoveredClass.slice(-1));
+        console.log(hoveredClass.slice(-1));
+        if (timesHovered < 9){
+            e.target.classList.remove(hoveredClass);
+            e.target.classList.add(`hovered-${timesHovered + 1}`);
+        }
+    }
+}
 
 function createGrid(numSquares){
     for (let i = 0; i < numSquares; i++){
