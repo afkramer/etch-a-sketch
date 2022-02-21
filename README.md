409439819e950efdc7d4bebafa5447c81b1febe8
Adriana Kramer <adri.kramer@gmail.com>
2022-02-21 20:47:58 +0100

    Add reset button function and README file text

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # etch-a-sketch
-My solution to the Etch a Sketch project from The Odin Project
+
+## My solution to the Etch a Sketch project for The Odin Project
+
+In this project I learned about different ways to create a grid of CSS objects. To hightlight the different paths I took until reaching the final product, I created 4 different branches. Each one contains the implementation of the grid using a different method, as suggested within the project instructions. It was really interesting to see how the same results could be acheived with 4 different methods!
+
+Event listeners were also a main focus of this topic. I created a function that changes a class on each grid item to change the color with each pass of the mouse until the darkest color is reached.
+
+I also tried to account for all the different types of input the user might provide after the reset button is pressed, as they could enter a word, hit cancel, leave the input box blank, etc. 
--- script.js
+++ script.js
@@ -6,24 +6,6 @@ gridContainer.style.width = `${gridSize}px`;
 
 createGrid(16);
 
-const gridItems = Array.from(document.querySelectorAll('.grid-item'));
-gridItems.forEach(item => item.addEventListener('mouseenter', changeColor));
-
-function changeColor(e){
-    const classes = Array.from(e.target.classList);
-    if (classes[2] === undefined){
-        e.target.classList.add('hovered-1');
-    } else {
-        const hoveredClass = classes[2];
-        const timesHovered = parseInt(hoveredClass.slice(-1));
-        console.log(hoveredClass.slice(-1));
-        if (timesHovered < 9){
-            e.target.classList.remove(hoveredClass);
-            e.target.classList.add(`hovered-${timesHovered + 1}`);
-        }
-    }
-}
-
 function createGrid(numSquares){
     for (let i = 0; i < numSquares; i++){
         const gridRow = document.createElement('div');
@@ -39,5 +21,44 @@ function createGrid(numSquares){
         }
 
         gridContainer.appendChild(gridRow);
+    }
+
+    const gridItems = Array.from(document.querySelectorAll('.grid-item'));
+    gridItems.forEach(item => item.addEventListener('mouseenter', changeColor));
+}
+
+
+function changeColor(e){
+    const classes = Array.from(e.target.classList);
+    if (classes[2] === undefined){
+        e.target.classList.add('hovered-1');
+    } else {
+        // The first two classes are 'grid-item' and 'grid', as defined above
+        // The third class would be the option to style after hovering
+        const hoveredClass = classes[2];
+        const timesHovered = parseInt(hoveredClass.slice(-1));
+        if (timesHovered < 9){
+            e.target.classList.remove(hoveredClass);
+            e.target.classList.add(`hovered-${timesHovered + 1}`);
+        }
+    }
+}
+
+function resetGrid(){
+    let numSquares = window.prompt("How many squares should each side have?\r\nEnter a number between 1 and 100.", 16);
+    // Only reset the grid if the user hasn't canceled
+    if (numSquares !== null && numSquares !== ''){
+        numSquares = parseInt(numSquares);
+        if (Number.isNaN(numSquares)){
+            alert("That was not a number. Setting default size of 16");
+            numSquares = 16;
+        } else if (numSquares > 100){
+            alert("The highest value possible is 100. Setting grid size of 100.");
+            numSquares = 100;
+        }
+
+        const gridRows = document.querySelectorAll('.grid-row');
+        gridRows.forEach(gridRow => gridContainer.removeChild(gridRow));
+        createGrid(numSquares);
     }
 }
